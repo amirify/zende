6b2c94e40d9ec773b8b325aa7fd6dd0d43d4346d
amirify <25364126+amirify@users.noreply.github.com>
2024-09-27 19:21:59 +0330

    setup basic structure

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: 'src/components/index.ts',
+    input: 'src/index.ts',
     output: [
         {
             file: 'dist/index.cjs.js',
