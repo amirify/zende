f7df2a16ed6bf1927ccb21b5860293c0274bde6c
amirify <25364126+amirify@users.noreply.github.com>
2024-11-06 01:33:51 +0330

    feat: add prettier and eslint-config-prettier and disable eslint formatting functions

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,19 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic,
     {
+        files: ["src/**/*.{js,jsx,ts,tsx}"],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.strict,
+            ...tseslint.configs.stylistic,
+        ],
         ignores: [
             "src/utils/themeUtils.ts"
         ]
-    }
+    },
+    eslintConfigPrettier
 );
